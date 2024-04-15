b34e0575c40d8384660496bf01f25a5469472cbb
Shubham-1020 <sk200310y@gmail.com>
2024-04-15 15:05:39 +0530

    Added the CSS to it

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -1,8 +1,9 @@
+import "./pokemon.css"
 function Pokemon({name,image}){
     return (
-        <div>
-             <div>{name}</div>
-             <div><img src={image}/></div>
+        <div className="pokemon">
+             <div className="pokemon-name">{name}</div>
+             <div><img className="pokemon-img" src={image}/></div>
         </div>
        
 
--- src/components/pokemonList/PokemonList.jsx
+++ src/components/pokemonList/PokemonList.jsx
@@ -42,8 +42,14 @@ function PokemonList(){
     return (
         <>
         <div className="pokemon-list-wrapper">
-            <div>Pokemon List </div>
-            {(isLoading) ? 'Loading...' : pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id}/>)}
+            
+            <div className="pokemon-wrapper">
+                {(isLoading) ? 'Loading...' : pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id}/>)}
+            </div>
+            <div className="controls">
+                <button>Previous</button>
+                <button>Next</button>
+            </div>
         </div>
         </>
     )
